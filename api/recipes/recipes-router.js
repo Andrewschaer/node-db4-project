96cd2341cbbb0a968a8a57dfdfcb08ac1c712245
Andrewschaer <andrewschaer@github.com>
2021-09-09 11:31:32 -0400

    get recipe endpoint built in recipes-router

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -4,8 +4,12 @@ const Recipe = require('./recipes-model');
 const router = express.Router();
 
 router.get('/:recipe_id', async (req, res, next) => {
-    const result = await Recipe.getRecipeById(req.params.recipe_id)
-    res.json(`TBD ENDPOINT RESULT - THIS IS REQ-PARAMS-RECIPE_ID: ${req.params.recipe_id} - THIS IS RESULT: ${result}`);
+    const { recipe_id } = req.params;
+    Recipe.getRecipeById(recipe_id)
+        .then(recipe => {
+        res.status(200).json(recipe)
+        })
+        .catch(next)
 });
 
 module.exports = router;
